8ac478c6a9f603dd2e8a6ad7bf004f3eced0772d
Marcin Marciniuk <marcinm222@gmail.com>
2024-05-27 00:17:52 +0200

    production vercel url

--- lib/utils.ts
+++ lib/utils.ts
@@ -7,7 +7,8 @@ export function cn(...inputs: ClassValue[]) {
 
 export function getBaseUrl() {
   if (typeof window !== "undefined") return "";
-  if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`;
+  if (process.env.VERCEL_URL)
+    return `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`;
   return `http://localhost:${process.env.PORT ?? 3000}`;
 }
 
