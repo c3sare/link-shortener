2da5489578f06f5fd2b83f82ad8a9b9027a2be10
Marcin Marciniuk <marcinm222@gmail.com>
2024-06-04 19:51:01 +0200

    update deps, add react compiler

--- components/dropdown-menu-server-action-item.tsx
+++ components/dropdown-menu-server-action-item.tsx
@@ -1,14 +1,19 @@
 import { DropdownMenuItem } from "@/components/ui/dropdown-menu";
-import { cn } from "@/lib/utils";
 
-type Props = React.DetailedHTMLProps<React.FormHTMLAttributes<HTMLFormElement>, HTMLFormElement>
+type Props = React.DetailedHTMLProps<
+  React.FormHTMLAttributes<HTMLFormElement>,
+  HTMLFormElement
+>;
 
-export const DropdownMenuServerActionItem = ({ children, action, className, ...rest }: Props) => (
-    <form action={action} {...rest}>
-        <DropdownMenuItem className={className} asChild>
-            <button>
-                {children}
-            </button>
-        </DropdownMenuItem>
-    </form>
-)
+export const DropdownMenuServerActionItem = ({
+  children,
+  action,
+  className,
+  ...rest
+}: Props) => (
+  <form action={action} {...rest}>
+    <DropdownMenuItem className={className} asChild>
+      <button>{children}</button>
+    </DropdownMenuItem>
+  </form>
+);
