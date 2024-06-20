627381fbbe979750746c96db12a8f2468ae194b6
C3sare <marcinm222@gmail.com>
2024-06-20 11:00:45 +0200

    delete edge runtime

--- app/l/[str]/route.tsx
+++ app/l/[str]/route.tsx
@@ -5,7 +5,7 @@ import { headers } from "next/headers";
 import { notFound, redirect } from "next/navigation";
 import { type NextRequest } from "next/server";
 
-export const runtime = "edge";
+// export const runtime = "edge";
 
 export async function GET(
   req: NextRequest,
