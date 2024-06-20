5652d8b589dd85ec2911f1ae6216421384947cbb
C3sare <marcinm222@gmail.com>
2024-06-20 11:18:26 +0200

    vercel project production url fix

--- lib/utils.ts
+++ lib/utils.ts
@@ -7,7 +7,7 @@ export function cn(...inputs: ClassValue[]) {
 
 export function getBaseUrl() {
   if (typeof window !== "undefined") return "";
-  if (process.env.VERCEL_URL)
+  if (process.env.VERCEL_PROJECT_PRODUCTION_URL)
     return `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`;
   return `http://localhost:${process.env.PORT ?? 3000}`;
 }
