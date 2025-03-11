5632f623123f8092778cf5364f55e8f6a6c9cc72
Marcin Marciniuk <marcinm222@gmail.com>
2025-03-11 20:29:22 +0100

    update packages

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -20,7 +20,7 @@ const eslintConfig = [
     "plugin:@typescript-eslint/recommended",
     "plugin:import/recommended",
     "plugin:import/typescript",
-    "plugin:playwright/recommended",
+    "plugin:playwright/recommended"
   ),
   {
     plugins: {
