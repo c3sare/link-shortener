2475933eddbe4257a774794c5ae8841a792d931e
Marcin Marciniuk <marcinm222@gmail.com>
2025-04-16 20:46:15 +0200

    cypress configuration

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,14 +1,17 @@
 import { defineConfig } from "cypress";
 
 export default defineConfig({
-  projectId: "psmecz",
   component: {
     devServer: {
       framework: "next",
       bundler: "webpack",
     },
   },
+
   e2e: {
-    // setupNodeEvents(on, config) {},
+    baseUrl: "http://localhost:3000",
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
   },
 });
