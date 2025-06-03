9bc9b488abd49986cac746a0bc2c5cf442e8ba5c
Marcin Marciniuk <marcinm222@gmail.com>
2025-06-03 15:29:40 +0200

    update packages

--- actions/safe-action.ts
+++ actions/safe-action.ts
@@ -1,10 +1,7 @@
 import { createSafeActionClient } from "next-safe-action";
-import { valibotAdapter } from "next-safe-action/adapters/valibot";
 import { auth } from "@/auth";
 
-export const action = createSafeActionClient({
-  validationAdapter: valibotAdapter(),
-});
+export const action = createSafeActionClient();
 
 export const authAction = action.use(async (req) => {
   const session = await auth();
