d8c7f7e73593cc2440ee26ee37a69c1f3e7bdf76
Marcin Marciniuk <marcinm222@gmail.com>
2025-10-03 10:10:18 +0200

    update packages

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,6 @@ import "./globals.css";
 
 import { Suspense } from "react";
 
-export const runtime = "edge";
-
 // export const experimental_ppr = true;
 
 export default function RootLayout({ children }: React.PropsWithChildren) {
